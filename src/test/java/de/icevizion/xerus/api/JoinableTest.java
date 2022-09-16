7ff964c8e703b118b6dbaa0d5004f40a0eaa7958
theEvilReaper <steffenwx@gmail.com>
2022-09-16 09:38:51 +0200

    Update import for the TestJoinableImpl class

--- src/test/java/de/icevizion/xerus/api/JoinableTest.java
+++ src/test/java/de/icevizion/xerus/api/JoinableTest.java
@@ -1,5 +1,6 @@
 package de.icevizion.xerus.api;
 
+import de.icevizion.xerus.api.mocks.TestJoinableImpl;
 import net.minestom.server.entity.Player;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
