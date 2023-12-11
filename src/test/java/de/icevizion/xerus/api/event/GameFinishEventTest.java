895b9484f5b9af6ea1514ee3f712ef51fc93d126
theEvilReaper <steffenwx@gmail.com>
2023-12-11 10:32:08 +0100

    Add test class for the GameFinishEvent

---
+++ src/test/java/de/icevizion/xerus/api/event/GameFinishEventTest.java
@@ -0,0 +1,38 @@
+package de.icevizion.xerus.api.event;
+
+import net.minestom.server.event.EventDispatcher;
+import net.minestom.testing.Env;
+import net.minestom.testing.EnvTest;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@EnvTest
+class GameFinishEventTest {
+
+    @Test
+    void testEventConstruction() {
+        var gameFinishEvent = new GameFinishEvent<>(FinishReason.UNKNOWN);
+        assertNotNull(gameFinishEvent);
+        assertEquals(FinishReason.class, gameFinishEvent.getReason().getClass());
+    }
+
+    @Test
+    void testEventFire(@NotNull Env env) {
+        var listener = env.listen(GameFinishEvent.class);
+
+        listener.followup(gameFinishEvent -> {
+            assertNotEquals(FinishReason.TEAM_WIN, gameFinishEvent.getReason());
+            assertEquals(FinishReason.ALL_DEAD, gameFinishEvent.getReason());
+        });
+
+        EventDispatcher.call(new GameFinishEvent<>(FinishReason.ALL_DEAD));
+
+    }
+
+    private enum FinishReason {
+        ALL_DEAD, UNKNOWN, TEAM_WIN
+    }
+
+}
