18779a74f15863186a8e561e5c5fa83c56943cba
theEvilReaper <steffenwx@gmail.com>
2024-02-22 22:44:08 +0100

    Upgrade dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,12 +3,15 @@ rootProject.name = "Xerus"
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            library("minestom", "net.onelitefeather.microtus", "Minestom").version("1.3.1")
-            library("minestom-test", "net.onelitefeather.microtus.testing", "testing").version("1.1.1")
-            library("junit-jupiter", "org.junit.jupiter", "junit-jupiter").version("5.10.2")
-            library("junit-jupiter-engine", "org.junit.jupiter", "junit-jupiter-engine").version("5.10.2")
-            library("mockito-core", "org.mockito", "mockito-core").version("5.7.0")
-            library("mockito-junit", "org.mockito", "mockito-junit-jupiter").version("5.7.0")
+            version("minestom", "1.3.1")
+            version("junit", "5.10.2")
+            version("mockito", "5.10.0")
+            library("minestom", "net.onelitefeather.microtus", "Minestom").versionRef("minestom")
+            library("minestom-test", "net.onelitefeather.microtus.testing", "testing").versionRef("minestom")
+            library("junit-jupiter", "org.junit.jupiter", "junit-jupiter").versionRef("junit")
+            library("junit-jupiter-engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
+            library("mockito-core", "org.mockito", "mockito-core").versionRef("mockito")
+            library("mockito-junit", "org.mockito", "mockito-junit-jupiter").versionRef("mockito")
             library("aves", "de.icevizion.lib", "Aves").version("1.3.1+dc3a1da8")
             library("strigiformes", "com.github.PatrickZdarsky", "Strigiformes").version("e89dd8352c")
 
