8b9684f5aaffba87de6f966b05d6f51ad0a08da4
theEvilReaper <steffenwx@gmail.com>
2024-03-23 15:51:59 +0100

    Update repository usage

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,37 +16,6 @@ java {
     }
 }
 
-repositories {
-    mavenCentral()
-    maven("https://jitpack.io")
-    maven {
-        val groupdId = "dungeon" // Gitlab Group
-        url = if (System.getenv().containsKey("CI")) {
-            val ciApiv4Url = System.getenv("CI_API_V4_URL")
-            uri("$ciApiv4Url/groups/$groupdId/-/packages/maven")
-        } else {
-            uri("https://gitlab.themeinerlp.dev/api/v4/groups/$groupdId/-/packages/maven")
-        }
-        name = "GitLab"
-        credentials(HttpHeaderCredentials::class.java) {
-            name = if (System.getenv().containsKey("CI")) {
-                "Job-Token"
-            } else {
-                "Private-Token"
-            }
-            value = if (System.getenv().containsKey("CI")) {
-                System.getenv("CI_JOB_TOKEN")
-            } else {
-                val gitLabPrivateToken: String? by project
-                gitLabPrivateToken
-            }
-        }
-        authentication {
-            create<HttpHeaderAuthentication>("header")
-        }
-    }
-}
-
 dependencies {
     implementation(libs.strigiformes)
 
