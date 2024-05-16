42f921101a9701ca8e17b4e0c5ff6328e754ffe6
theEvilReaper <steffenwx@gmail.com>
2024-05-16 11:25:06 +0200

    Improve dependency handling

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,11 +3,13 @@ rootProject.name = "zosma" // TODO("Change me ;D")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            library("minestom", "net.onelitefeather.microtus", "Minestom").version("1.3.1")
-            library("minestom-test", "net.onelitefeather.microtus.testing", "testing").version("1.1.1")
+            version("minestom", "1.3.1")
+            version("junit", "5.10.2")
+            library("minestom", "net.onelitefeather.microtus", "Minestom").versionRef("minestom")
+            library("minestom-test", "net.onelitefeather.microtus.testing", "testing").versionRef("minestom")
             library("adventure.minimessage", "net.kyori", "adventure-text-minimessage").version("4.14.0")
-            library("junit.api", "org.junit.jupiter", "junit-jupiter-api").version("5.10.2")
-            library("junit.engine", "org.junit.jupiter", "junit-jupiter-engine").version("5.10.2")
+            library("junit.api", "org.junit.jupiter", "junit-jupiter-api").versionRef("junit")
+            library("junit.engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
             library("mockito.core", "org.mockito", "mockito-core").version("5.6.0")
             library("mockito.junit", "org.mockito", "mockito-junit-jupiter").version("5.6.0")
 
