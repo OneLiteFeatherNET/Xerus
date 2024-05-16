42b29d40cd7f59fac61a5cde7c28470e1e047de4
theEvilReaper <steffenwx@gmail.com>
2024-05-16 11:37:47 +0200

    Improve version handling

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,13 +5,14 @@ dependencyResolutionManagement {
         create("libs") {
             version("minestom", "1.3.1")
             version("junit", "5.10.2")
+            version("mockito", "5.12.0")
             library("minestom", "net.onelitefeather.microtus", "Minestom").versionRef("minestom")
             library("minestom-test", "net.onelitefeather.microtus.testing", "testing").versionRef("minestom")
-            library("adventure.minimessage", "net.kyori", "adventure-text-minimessage").version("4.14.0")
+            library("adventure.minimessage", "net.kyori", "adventure-text-minimessage").version("4.17.0")
             library("junit.api", "org.junit.jupiter", "junit-jupiter-api").versionRef("junit")
             library("junit.engine", "org.junit.jupiter", "junit-jupiter-engine").versionRef("junit")
-            library("mockito.core", "org.mockito", "mockito-core").version("5.6.0")
-            library("mockito.junit", "org.mockito", "mockito-junit-jupiter").version("5.6.0")
+            library("mockito.core", "org.mockito", "mockito-core").versionRef("mockito")
+            library("mockito.junit", "org.mockito", "mockito-junit-jupiter").versionRef("mockito")
 
             plugin("shadow", "com.github.johnrengelman.shadow").version("8.1.1")
             plugin("sonar", "org.sonarqube").version("4.0.0.2929")
