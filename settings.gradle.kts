0be2d5ac0e6854d1270c24407df755ab1f03b0c2
theEvilReaper <steffenwx@gmail.com>
2025-07-08 10:36:58 +0200

    Updates Aves to the latest release

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -30,7 +30,7 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             version("bom", "1.3.0")
-            version("aves", "1.8.0")
+            version("aves", "1.9.0")
             version("publishdata", "1.4.0")
 
             library("bom.base", "net.onelitefeather", "mycelium-bom").versionRef("bom")
