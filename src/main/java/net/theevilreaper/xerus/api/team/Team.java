61288cda5dc9eac388634bbaef7293b8480a0231
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-27 16:09:39 +0200

    Update event calls

--- src/main/java/net/theevilreaper/xerus/api/team/Team.java
+++ src/main/java/net/theevilreaper/xerus/api/team/Team.java
@@ -7,6 +7,7 @@ import net.theevilreaper.xerus.api.team.event.PlayerTeamEvent;
 import net.kyori.adventure.text.Component;
 import net.minestom.server.entity.Player;
 import net.minestom.server.event.EventDispatcher;
+import net.theevilreaper.xerus.api.team.event.TeamAction;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -19,16 +20,19 @@ import java.util.function.Consumer;
 /**
  * The Team interface represents a blueprint for managing a team or a similar group in a software application.
  * It defines essential methods for organizing and interacting with a team of players or members.
+ *
  * @author theEvilReaper
- * @version 1.0.3
+ * @version 1.0.4
  * @since 1.1.6
  **/
 public interface Team extends Joinable {
 
-    @NotNull Runnable EMPTY = () -> { };
+    @NotNull Runnable EMPTY = () -> {
+    };
 
     /**
      * Creates a new instance from the {@link TeamImpl.Builder} interface.
+     *
      * @return the created instance
      */
     @Contract(value = " -> new", pure = true)
@@ -39,6 +43,7 @@ public interface Team extends Joinable {
     /**
      * Creates a new instance from a {@link Team.Builder} to create a new team.
      * This method allows to set a {@link TeamCreator} to create custom teams over the builder
+     *
      * @param creator the creator to use
      * @return the builder instance
      */
@@ -49,7 +54,8 @@ public interface Team extends Joinable {
 
     /**
      * Creates a new instance from the {@link Builder} to create a team.
-     * @param name the name of the team
+     *
+     * @param name      the name of the team
      * @param colorData the {@link ColorData} to set
      * @return the created instance
      */
@@ -60,9 +66,10 @@ public interface Team extends Joinable {
 
     /**
      * Creates a new instance from the {@link Builder} to create a team.
-     * @param name the name of the team
+     *
+     * @param name      the name of the team
      * @param colorData the {@link ColorData} to set
-     * @param capacity the capacity of the team
+     * @param capacity  the capacity of the team
      * @return the created instance
      */
     @Contract(value = "_, _, _ -> new", pure = true)
@@ -72,20 +79,22 @@ public interface Team extends Joinable {
 
     /**
      * Add a single {@link Player} entry to a structure.
-     * @param player the player to add
+     *
+     * @param player   the player to add
      * @param consumer a consumer which is called to execute some logic
      */
     @Override
     default void addPlayer(@NotNull Player player, @Nullable Consumer<Player> consumer) {
         if (getPlayers().add(player)) {
-            var teamEvent = PlayerTeamEvent.addEvent(player, this);
+            PlayerTeamEvent teamEvent = new PlayerTeamEvent(player, this, TeamAction.ADD);
             EventDispatcher.callCancellable(teamEvent, consumer != null ? () -> consumer.accept(player) : EMPTY);
         }
     }
 
     /**
      * Add certain players to a team.
-     * @param players the set with the players to add
+     *
+     * @param players  the set with the players to add
      * @param consumer a consumer which is called to execute some logic
      */
     @Override
@@ -93,25 +102,27 @@ public interface Team extends Joinable {
         if (players.isEmpty()) return;
         players.removeIf(player -> !getPlayers().add(player));
         Runnable successCallback = consumer == null ? EMPTY : () -> getPlayers().forEach(consumer);
-        EventDispatcher.callCancellable(MultiPlayerTeamEvent.addEvent(this, players), successCallback);
+        EventDispatcher.callCancellable(new MultiPlayerTeamEvent(this, players, TeamAction.ADD), successCallback);
     }
 
     /**
      * Remove a single {@link Player} entry from a structure.
+     *
      * @param paramPlayer the player to remove
-     * @param consumer a consumer which is called to execute some logic
+     * @param consumer    a consumer which is called to execute some logic
      */
     @Override
     default void removePlayer(@NotNull Player paramPlayer, @Nullable Consumer<Player> consumer) {
         if (getPlayers().remove(paramPlayer)) {
-            var event = PlayerTeamEvent.removeEvent(paramPlayer, this);
-            EventDispatcher.callCancellable(event, consumer == null ? EMPTY : () -> consumer.accept(paramPlayer));
+            PlayerTeamEvent teamEvent = new PlayerTeamEvent(paramPlayer, this, TeamAction.REMOVE);
+            EventDispatcher.callCancellable(teamEvent, consumer == null ? EMPTY : () -> consumer.accept(paramPlayer));
         }
     }
 
     /**
      * Remove certain players from a team.
-     * @param players The set of players to remove
+     *
+     * @param players  The set of players to remove
      * @param consumer a consumer which is called to execute some logic
      */
     @Override
@@ -119,25 +130,28 @@ public interface Team extends Joinable {
         if (players.isEmpty()) return;
         players.removeIf(player -> getPlayers().contains(player));
         Runnable successCallback = consumer == null ? EMPTY : () -> getPlayers().forEach(consumer);
-        EventDispatcher.callCancellable(MultiPlayerTeamEvent.removeEvent(this, players), successCallback);
+        EventDispatcher.callCancellable( new MultiPlayerTeamEvent(this, players, TeamAction.REMOVE), successCallback);
     }
 
     /**
      * Set / overwrite the capacity of the team.
-     * @param capacity The capacity to set
+     *
+     * @param capacity the capacity to set
      */
     void setCapacity(int capacity);
 
     /**
      * Checks if a player can join a team or not.
+     *
      * @return true when a player can join otherwise false
      */
     boolean canJoin();
 
     /**
      * Checks if a given player is in the team.
-     * @param player The player to check
-     * @return True when the player is in the team otherwise false
+     *
+     * @param player the player to check
+     * @return true when the player is in the team otherwise false
      */
     default boolean hasPlayer(@NotNull Player player) {
         return getPlayers().contains(player);
@@ -145,7 +159,8 @@ public interface Team extends Joinable {
 
     /**
      * Send a specific message to each player in the team.
-     * @param component The message wraps as {@link Component} who should send
+     *
+     * @param component the message wraps as {@link Component} who should send
      */
     default void sendMessage(@NotNull Component component) {
         if (isEmpty()) return;
@@ -164,33 +179,39 @@ public interface Team extends Joinable {
 
     /**
      * Returns the identifier of the team.
+     *
      * @return the underlying value
      */
     @NotNull String getIdentifier();
 
     /**
-     * Returns the name of the team.
-     * @return The name of the team
+     * Return the name of the team based on the given locale.
+     *
+     * @param locale the locale to use for the name
+     * @return the name of the team
      */
-    @UnknownNullability Component getName(Locale locale);
+    @UnknownNullability
+    Component getName(Locale locale);
 
     /**
-     * Returns the name of the team.
-     * @return The name of the team
+     * @return the given name
      */
     default @NotNull Component getName() {
         return getName(null);
     }
 
     /**
-     * Returns the name with the color.
-     * @return The name with the color
+     * Returns a colored name based on the given locale.
+     *
+     * @return the colored name
      */
-    @UnknownNullability Component getColoredName(Locale locale);
+    @UnknownNullability
+    Component getColoredName(Locale locale);
 
     /**
      * Returns the name with the color.
-     * @return The name with the color
+     *
+     * @return the colored name
      */
     default @NotNull Component getColoredName() {
         return getColoredName(null);
@@ -198,12 +219,14 @@ public interface Team extends Joinable {
 
     /**
      * Returns the given color data from the team.
+     *
      * @return the color data
      */
     @NotNull ColorData getColorData();
 
     /**
      * Returns a boolean indicator if the team contains entries or not.
+     *
      * @return true when the team has no entries otherwise false
      */
     default boolean isEmpty() {
@@ -212,18 +235,21 @@ public interface Team extends Joinable {
 
     /**
      * Returns the maximum capacity of the team.
+     *
      * @return -1 when no capacity is set otherwise the capacity
      */
     int getCapacity();
 
     /**
      * Returns the current size of the team.
+     *
      * @return the current size
      */
     int getCurrentSize();
 
     /**
      * Returns a set which includes all current players in the team
+     *
      * @return the given set
      */
     @NotNull Set<Player> getPlayers();
@@ -235,6 +261,7 @@ public interface Team extends Joinable {
 
         /**
          * Set a name to the team.
+         *
          * @param name the new name set
          * @return the builder instance
          */
@@ -242,6 +269,7 @@ public interface Team extends Joinable {
 
         /**
          * Set the used color to the team
+         *
          * @param colorData the {@link ColorData} to set
          * @return the builder instance
          */
@@ -249,6 +277,7 @@ public interface Team extends Joinable {
 
         /**
          * Set the maximum size for the team.
+         *
          * @param capacity the capacity to set
          * @return the builder instance
          */
@@ -257,6 +286,7 @@ public interface Team extends Joinable {
         /**
          * Creates a new instance from the team with the given values from the builder.
          * When some value is invalid the creation process will fail with an exception
+         *
          * @return the builder instance
          */
         @NotNull Team build();
