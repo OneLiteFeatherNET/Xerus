bf4b9a69cafafb32c8d8f9ee5490327aceac3920
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-27 15:18:14 +0200

    Remove the ability to change the name after a phase is created

--- src/main/java/net/theevilreaper/xerus/api/phase/Phase.java
+++ src/main/java/net/theevilreaper/xerus/api/phase/Phase.java
@@ -1,34 +1,36 @@
 package net.theevilreaper.xerus.api.phase;
 
-import net.minestom.server.utils.validate.Check;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author Patrick Zdarsky / Rxcki
  * @version 1.0
  * @since 03/01/2020 21:00
- *
+ * <br>
  * Describes any Phase which will be externally started and which can finish at some point,
  * when the phase finishes it can notify a listener using the {@code finishedCallback}
  */
 public abstract class Phase {
 
-    private String name = "Unnamed phase";
+    private final String name;
     private boolean running;
     private boolean finished;
     private boolean skipping;
 
     private Runnable finishedCallback;
 
-    protected Phase() { }
-
+    /**
+     * Creates a new reference of the {@link Phase} with the given parameters.
+     *
+     * @param name the name of the phase, used to identify it
+     */
     protected Phase(@NotNull String name) {
         this.name = name;
     }
 
     /**
      * Used to start the phase.
-     *
+     * <p>
      * A phase can only be started once, so when running is true the phase won't be started
      */
     public void start() {
@@ -41,11 +43,12 @@ public abstract class Phase {
 
     protected abstract void onStart();
 
-    public void onSkip() {}
+    public void onSkip() {
+    }
 
     /**
      * Used to finish the phase.
-     *
+     * <p>
      * A phase can only be finished once, so when finished is true the phase won't be stated as finished again
      */
     public void finish() {
@@ -63,16 +66,12 @@ public abstract class Phase {
         return this;
     }
 
-    public void setName(@NotNull String name) {
-        Check.argCondition(name.trim().isEmpty(), "The name can't be empty");
-        this.name = name;
-    }
-
     /**
-     * @return The name of this phase
+     * Returns the name representing this phase.
+     *
+     * @return the given name
      */
-    @NotNull
-    public String getName() {
+    public @NotNull String getName() {
         return name;
     }
 
@@ -104,6 +103,7 @@ public abstract class Phase {
 
     /**
      * Sets the callback which will be called when the phase is finished
+     *
      * @param finishedCallback The callback to be called when the phase finished
      */
     public void setFinishedCallback(Runnable finishedCallback) {
