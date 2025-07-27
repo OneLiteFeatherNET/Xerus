2c08fcaa217f32de9da0ac699859e1277b49b494
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-07-27 20:03:08 +0200

    Add documentation

--- src/main/java/net/theevilreaper/xerus/api/team/event/TeamAction.java
+++ src/main/java/net/theevilreaper/xerus/api/team/event/TeamAction.java
@@ -1,7 +1,20 @@
 package net.theevilreaper.xerus.api.team.event;
 
+/**
+ * The {@link TeamAction} enum defines the possible actions that can be performed on a team.
+ *
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.7.3
+ */
 public enum TeamAction {
 
+    /**
+     * Represents the action of adding a player to a team.
+     */
     ADD,
+    /**
+     * Represents the action of removing a player from a team.
+     */
     REMOVE;
 }
