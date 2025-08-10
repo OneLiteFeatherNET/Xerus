7fcaebc79b6d0d077edf1f528ae5c4c714f794b7
theEvilReaper <theEvilReaper@users.noreply.github.com>
2025-08-10 21:55:23 +0200

    Add key usage to the kit part

--- src/main/java/net/theevilreaper/xerus/api/kit/KitService.java
+++ src/main/java/net/theevilreaper/xerus/api/kit/KitService.java
@@ -1,14 +1,17 @@
 package net.theevilreaper.xerus.api.kit;
 
+import net.kyori.adventure.key.Key;
+import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.UnmodifiableView;
 
 import java.util.List;
 import java.util.Optional;
 
 /**
  * The interface includes all relevant method to handle the kits for a given implementation.
- * When the developer does not want to use the {@link KitServiceImpl} implementation.
+ * When the developer does not want to use the {@link DefaultKitService} implementation.
  * So he must implement the interface by self and can add additional method to the service.
  *
  * @author theEvilReaper
@@ -16,6 +19,16 @@ import java.util.Optional;
  * @since 1.2.0
  **/
 public interface KitService {
+
+    /**
+     * Returns a new instance of the {@link KitService}.
+     *
+     * @return the new instance
+     */
+    @Contract(pure = true)
+    static @NotNull KitService of() {
+        return new DefaultKitService();
+    }
 
     /**
      * Add a kit to the service.
@@ -38,7 +51,7 @@ public interface KitService {
      * @param identifier the identifier from the kit
      * @return true when the kit can be removed otherwise false
      */
-    boolean remove(@NotNull String identifier);
+    boolean remove(@NotNull Key identifier);
 
     /**
      * Clears the underlying data structure.
@@ -51,12 +64,15 @@ public interface KitService {
      * @param name the name of the kit
      * @return the fetched kit in an optional
      */
-    @NotNull Optional<@Nullable Kit> getKit(@NotNull String name);
+    @NotNull Optional<@Nullable Kit> getKit(@NotNull Key name);
 
     /**
      * Returns a list with all current available kits.
      *
      * @return the underlying list
      */
-    @NotNull List<Kit> getKits();
+    @NotNull
+    @UnmodifiableView
+    @Contract(pure = true)
+    List<Kit> getKits();
 }
