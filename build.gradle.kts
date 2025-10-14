663af79728beee0deb1e64d48231360a9f240ef9
Steffen Wonning <6805083+theEvilReaper@users.noreply.github.com>
2025-10-14 10:11:25 +0200

    Remove publishdata plugin usage (#35)
    * Remove publishdata plugin definition
    
    * Migrate publishing section
    
    ---------
    
    Co-authored-by: theEvilReaper <theEvilReaper@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     `java-library`
     `maven-publish`
     jacoco
-    alias(libs.plugins.publishdata)
 }
 
 group = "net.theevilreaper"
@@ -55,35 +54,25 @@ tasks {
     }
 }
 
-publishData {
-    addMainRepo("https://repo.onelitefeather.dev/onelitefeather-releases")
-    addMasterRepo("https://repo.onelitefeather.dev/onelitefeather-releases")
-    addSnapshotRepo("https://repo.onelitefeather.dev/onelitefeather-snapshots")
-    publishTask("jar")
-}
-
-
 publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            // configure the publication as defined previously.
-            publishData.configurePublication(this)
-            version = publishData.getVersion(false)
-        }
+    publications.create<MavenPublication>("maven") {
+        from(components["java"])
     }
+
     repositories {
         maven {
             authentication {
                 credentials(PasswordCredentials::class) {
-                    // Those credentials need to be set under "Settings -> Secrets -> Actions" in your repository
                     username = System.getenv("ONELITEFEATHER_MAVEN_USERNAME")
                     password = System.getenv("ONELITEFEATHER_MAVEN_PASSWORD")
                 }
             }
-
             name = "OneLiteFeatherRepository"
-            // Get the detected repository from the publish data
-            url = uri(publishData.getRepository())
+            url = if (project.version.toString().contains("SNAPSHOT")) {
+                uri("https://repo.onelitefeather.dev/onelitefeather-snapshots")
+            } else {
+                uri("https://repo.onelitefeather.dev/onelitefeather-releases")
+            }
         }
     }
 }
