73d02d9f103ed9e8d551b270394c52b62968ae25
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-06-14 13:56:47 +0200

    chore(deps): update mycelium-bom version to 1.7.1

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,7 +21,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            version("bom", "1.7.0")
+            version("bom", "1.7.1")
             version("slf4j", "2.0.18")
 
             library("bom.base", "net.onelitefeather", "mycelium-bom").versionRef("bom")
