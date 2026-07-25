faad6f2bb88047b0acc33fb4f8ec3e926e1e53a8
theEvilReaper <theEvilReaper@users.noreply.github.com>
2026-07-25 12:20:12 +0200

    feat(color): add method to translate a ColorData to a TeamColor

--- src/test/java/net/theevilreaper/xerus/api/ColorDataTest.java
+++ src/test/java/net/theevilreaper/xerus/api/ColorDataTest.java
@@ -3,8 +3,11 @@ package net.theevilreaper.xerus.api;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.minestom.server.color.Color;
 import net.minestom.server.color.DyeColor;
+import net.minestom.server.color.TeamColor;
 import net.minestom.server.item.Material;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,5 +28,17 @@ class ColorDataTest {
     @Test
     void testLength() {
         assertSame(16, ColorData.getValues().length);
+    }
+
+    @ParameterizedTest
+    @EnumSource(ColorData.class)
+    void testToTeamColorMatchesChatColor(ColorData colorData) {
+        TeamColor teamColor = colorData.toTeamColor();
+        NamedTextColor chatColor = colorData.getChatColor();
+
+        assertNotNull(teamColor);
+        assertEquals(chatColor.red(), teamColor.red());
+        assertEquals(chatColor.green(), teamColor.green());
+        assertEquals(chatColor.blue(), teamColor.blue());
     }
 }
